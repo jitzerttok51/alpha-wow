b1c57eee73b68bcd9fb17d0fc901a43785f37a02
Prodanov, Nikolay <jitzerttok98@gmail.com>
2022-03-26 21:49:02 +0200

    Initial commit

---
+++ src/main/java/org/deathkiller/wow/alpha/AlphaWowApplication.java
@@ -0,0 +1,12 @@
+package org.deathkiller.wow.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaWowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaWowApplication.class, args);
+	}
+}
